763ed2b87934b1c70a3d36800d291b73314b3d7b
suman <sumanbhowmik96sb@gmai.com>
2020-04-24 19:45:01 +0530

    ''

--- DJANGO_COURSE_2.xx/21-Social_Clone_Project/simplesocial/simplesocial/settings.py
+++ DJANGO_COURSE_2.xx/21-Social_Clone_Project/simplesocial/simplesocial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
